e85273a24e8efd0d37ced19e1381703350ab92de
Antoine Bdns <antoine.boudens@gmail.com>
2023-05-30 13:23:40 +0200

    Import Springboot project
    Created with spring initializer

---
+++ src/main/java/fr/antoinebdns/springbootapistarter/SpringbootapiStarterApplication.java
@@ -0,0 +1,13 @@
+package fr.antoinebdns.springbootapistarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapiStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootapiStarterApplication.class, args);
+	}
+
+}
