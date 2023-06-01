dd0fad75c72ec393f09c4d0d6e73e65dcf22c3f0
Antoine Bdns <antoine.boudens@gmail.com>
2023-06-01 13:34:17 +0200

    Add ControllerAdviceException

--- src/main/java/fr/antoinebdns/springbootapistarter/controllers/SampleController.java
+++ src/main/java/fr/antoinebdns/springbootapistarter/controllers/SampleController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,11 @@ public class SampleController {
     @GetMapping("/getSampleModel")
     public List<SampleModel> getSampleModelList(){
         return sampleService.findAllSamples();
+    }
+
+    @GetMapping("/getSampleModel/{id}")
+    public SampleModel getSampleModelById(@PathVariable("id")Long id){
+        return sampleService.findSample(id);
     }
 
 }
